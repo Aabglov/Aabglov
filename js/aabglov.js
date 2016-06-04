260ff8d7dbfbb373f5feaa7ae016515553898348
Kegan Rabil <trisectionage@gmail.com>
2016-06-04 15:53:21 -0600

    Added pretty much everything: First draft

---
+++ js/aabglov.js
@@ -0,0 +1,63 @@
+//
+
+var screen_height;
+var screen_width;
+var cal_height;
+var map_size = 0.8;
+var iOS = false;
+
+//window.onload = init;
+$(document).ready(init);
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+function init(){
+    // Determine if it's an iPhone
+    iOS = /iPad|iPhone|iPod/.test(navigator.platform);
+
+    screen_height = $(window).height()
+    screen_width = $(window).width()
+
+    // Get the em size
+    var em_width = screen_width / parseFloat($("body").css("font-size"));
+    console.log(em_width);
+
+    // Create the carousel bindings
+    // Bumping this value up because iPhone 6 is too big in landscape view
+    if(em_width <= 70.0){
+      console.log('MOBILE INIT');
+      mobileInit();
+    }
+
+    // Bind smooth scrolling
+    $(function() {
+        $('a.page-scroll').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
+}
+
+function mobileInit(){
+
+  window.onresize = function(event) {
+    screen_width = $(window).width()
+    screen_height = $(window).height();
+    // Perform iPhoneHack if the device is an iPhone
+    if(iOS){
+      iPhoneHack(screen_height,screen_width);
+    }
+  };
+
+  // Create Carousel
+  $('#cv').slick({
+      autoplay:false,
+      dots: true,
+      infinite: true,
+      slidesToShow: 1,
+      arrows:false
+    });
+}
