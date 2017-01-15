5485038783759fed160f93977d0177072c422075
Kegan Rabil <trisectionage@gmail.com>
2017-01-14 17:05:44 -0700

    Updated website and added test flask app

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
+# -*- coding: utf-8 -*-
+
 import os
+import logging
 from flask import Flask, render_template, request, Response, jsonify
 import json
+from functools import wraps
+import datetime
+import HTMLParser
+
 
 app = Flask(__name__)
-app.debug = True
+app.debug = True#'DEBUG' in os.environ
 
-@app.route('/')
-def aabglov():
-    return render_template('index.html')
 
-if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=443)
+
+######################### WEBSITE #########################
+def test(msg):
+    return "Your message: {}".format(msg.upper())
+
+@app.route('/',methods=["POST"])
+def hello():
+    data = request.json
+    clean_msg = HTMLParser.HTMLParser().unescape(data.get("msg"))
+    new_msg = test(clean_msg)
+    ret = {"status":200,"response":new_msg}
+    return jsonify(**ret)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
